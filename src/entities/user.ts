517da8066a2f50a678d26c8c5adc9fe819c0e73b
aosorio@possibilitycompany.com-at-743433884678 <akosoriod@unal.edu.co>
2022-02-25 00:19:59 -0500

    user info endpoint

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -2,24 +2,18 @@ import { getDB,  executeTransactWrite,getClient } from "../helpers/dynamoHelper"
 
 const TABLE_NAME = process.env.TABLE_NAME || "";
 
-export const userInfo = async (username:string): Promise<any> => {
-  const PK: string = "WALLET";
+export const user = async (username:string): Promise<any> => {
+  const PK: string = "USER";
   const SK: string = "USER#" + username;
   const params = {
       Key: {
           PK,
           SK
       },
-      TableName: TABLE_NAME,
-      ProjectionExpression: "#pk,#nft,#key",
-      ExpressionAttributeNames: {
-        "#pk": "PK",
-        "#nft": "firstNftClaimed",
-        "#key": "public_key",
-      }
+      TableName: TABLE_NAME
   };
   try {
-      return await getDB(params, "The NFT not exist");
+      return await getDB(params, "The user not exist");
     } catch (error) {
         console.log(error)
         throw error
--- src/handlers/user/users.ts
+++ src/handlers/user/users.ts
@@ -1,15 +1,16 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler } from "aws-lambda"
-import { userInfo } from "../../entities/user"
+import { user } from "../../entities/user"
 import { getResponse } from "../../helpers/lambdaResponseHelper";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
     try{
-       // let userData:any = await userInfo(user.username);
+        const { username } = event.pathParameters;
+        let userData:any = await user(username);
         return getResponse({
             statusCode: 200,
-            body: {
-                msg:"users"
-            }
+            body:  JSON.stringify({
+                userData
+            })
         })
     }catch (error){
         return getResponse({error})
